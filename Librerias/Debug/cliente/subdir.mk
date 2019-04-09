e2f7eda4c1d9e6939c763b554a8e3409f159e3be
fdalmaup <dalmaufacundo@gmail.com>
2019-04-09 00:17:37 -0300

    Debug

--- Librerias/Debug/cliente/subdir.mk
+++ Librerias/Debug/cliente/subdir.mk
@@ -4,13 +4,13 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../cliente/client.c 
+../cliente/cliente.c 
 
 OBJS += \
-./cliente/client.o 
+./cliente/cliente.o 
 
 C_DEPS += \
-./cliente/client.d 
+./cliente/cliente.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
