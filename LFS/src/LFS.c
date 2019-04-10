7d7ce111a1f41157230b53c74ccf6f67e7350106
juanmaalt <juanmaalt@gmail.com>
2019-04-09 23:54:13 -0300

    Config updates

--- LFS/src/LFS.c
+++ LFS/src/LFS.c
@@ -4,14 +4,34 @@
  Author      : 
  Version     :
  Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Description : Lissandra File System
  ============================================================================
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "LFS.h"
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	levantar_config();
+	ver_config();
+
 	return EXIT_SUCCESS;
+}
+
+
+void levantar_config() {
+	t_config *configFile = config_create("LFS.config");
+
+	config.puerto_escucha = config_get_string_value(configFile, "PUERTO_ESCUCHA");
+	config.punto_montaje = config_get_string_value(configFile, "PUNTO_MONTAJE");
+	config.retardo = config_get_string_value(configFile, "RETARDO");
+	config.tamanio_value = config_get_string_value(configFile, "TAMANIO_VALUE");
+	config.tiempo_dump = config_get_string_value(configFile, "TIEMPO_DUMP");
+
+	//free(configFile);
+}
+
+void ver_config(void){
+	printf("PUERTO_ESCUCHA=%s\nPUNTO_MONTAJE=%s\nRETARDO=%s\nTAMANIO_VALUE=%s\nTIEMPO_DUMP=%s\n", config.puerto_escucha, config.punto_montaje, config.retardo, config.tamanio_value, config.tiempo_dump);
 }
