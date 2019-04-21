4f5d22d2743ff413566a0ab8543bf6e3b8bdb905
Facu Salerno <facusalerno@MacBook-Air-de-Facu.local>
2019-04-20 22:04:52 -0300

    Refactorizacion 1

--- Bibliotecas_compartidas/server_multithread/server_multithread.h
+++
@@ -1,8 +0,0 @@
-#ifndef SERVER_MULTITHREAD_SERVER_MULTITHREAD_H_
-#define SERVER_MULTITHREAD_SERVER_MULTITHREAD_H_
-
-#include <pthread.h> //Para manejo de threads
-#include "../server/server.h" //Para enable_server, y en consecuencia todo lo que incluya server.h
-//server_multithread es un caso particular de server.c
-
-#endif /* SERVER_MULTITHREAD_SERVER_MULTITHREAD_H_ */
--- LFS/src/Memtable.h
+++
@@ -1,8 +0,0 @@
-#ifndef MEMTABLE_H
-#define MEMTABLE_H
-
-typedef struct {
-
-} Memtable;
-
-#endif // MEMTABLE_H
