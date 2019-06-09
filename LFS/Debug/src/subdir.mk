5c4d243f97d5216cccb0935d20e80fa17425d194
celemdr <mederoceleste@gmail.com>
2019-06-09 17:23:10 -0300

    Update

--- LFS/Debug/src/subdir.mk
+++ LFS/Debug/src/subdir.mk
@@ -4,20 +4,26 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../src/LFS.c 
+../src/APILissandra.c \
+../src/Consola.c \
+../src/Lissandra.c 
 
 OBJS += \
-./src/LFS.o 
+./src/APILissandra.o \
+./src/Consola.o \
+./src/Lissandra.o 
 
 C_DEPS += \
-./src/LFS.d 
+./src/APILissandra.d \
+./src/Consola.d \
+./src/Lissandra.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I"/home/utnso/workspace-tp-2019/Bibliotecas_compartidas" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	gcc -I"/home/facusalerno/workspace-tp-2019/Bibliotecas_compartidas" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- Memoria/Debug/src/subdir.mk
+++ Memoria/Debug/src/subdir.mk
@@ -4,12 +4,18 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
+../src/APIMemoria.c \
+../src/Consola.c \
 ../src/Memoria.c 
 
 OBJS += \
+./src/APIMemoria.o \
+./src/Consola.o \
 ./src/Memoria.o 
 
 C_DEPS += \
+./src/APIMemoria.d \
+./src/Consola.d \
 ./src/Memoria.d 
 
 
@@ -17,7 +23,7 @@ C_DEPS += \
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I"/home/utnso/workspace-tp-2019/Bibliotecas_compartidas" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	gcc -I"/home/facusalerno/workspace-tp-2019/Bibliotecas_compartidas" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
