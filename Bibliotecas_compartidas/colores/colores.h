49d836039d431775b658b1c45cfc64bec6c96ca6
facundosalerno <facundosalerno1@gmail.com>
2019-06-09 23:56:32 -0300

    agregados mas colores para darle onda

--- Bibliotecas_compartidas/colores/colores.h
+++ Bibliotecas_compartidas/colores/colores.h
@@ -8,7 +8,17 @@
 #ifndef COLORES_COLORES_H_
 #define COLORES_COLORES_H_
 
-#define RED "\x1b[31m"
+//Iniciar una cadena con un color y terminar siempre con STD
+//Ejemplo RED"Cadena"STD
+
 #define STD "\x1b[0m"
+#define RED "\x1b[31m"
+#define NRM  "\x1B[0m"
+#define GRN  "\x1B[32m"
+#define YEL  "\x1B[33m"
+#define BLU  "\x1B[34m"
+#define MAG  "\x1B[35m"
+#define CYN  "\x1B[36m"
+#define WHT  "\x1B[37m"
 
 #endif /* COLORES_COLORES_H_ */
