577ec4c3f606540b9c6ef3f32d6d4cab3bbbb16c
Celeste <mederoceleste@gmail.com>
2019-06-23 23:14:20 -0300

    implemento compactarArchivo

--- LFS/src/Compactador.c
+++ LFS/src/Compactador.c
@@ -14,3 +14,25 @@ void compactarTabla(char* nombreTabla) {
 		}
 	}
 }
+
+int isTmp(char* name) {
+	return strcmp(extension(name), "tmp") == 0;
+}
+
+char* extension(char *filename) {
+	char* dot = strrchr(filename, '.');
+	if (!dot || dot == filename) {
+		return "";
+	}
+	return dot + 1;
+}
+
+void compactarArchivo(FILE* file) {
+	char* line = NULL;
+	size_t len = 0;
+	while (getline(&line, &len, stream) != -1) {
+		Registro* registro = registro(line, len);
+		compactarRegistro(registro);
+		free(registro);
+	}
+}
