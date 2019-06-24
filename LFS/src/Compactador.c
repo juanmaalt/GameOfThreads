cfddc99f24ac947b0b473f8c93858d9b97b9a566
Celeste <mederoceleste@gmail.com>
2019-06-23 23:13:33 -0300

    implemento compactarTabla

--- LFS/src/Compactador.c
+++ LFS/src/Compactador.c
@@ -1,8 +1,16 @@
-/*
- * Compactador.c
- *
- *  Created on: 16 jun. 2019
- *      Author: juanmaalt
- */
 
+void compactarTabla(char* nombreTabla) {
+	char* path = malloc(100 * sizeof(char));
+	setPathTabla(path, nombreTabla);
+	DIR* dir = openddir(path);
+	struct dirent *de;
 
+	while ((de = readdir(dir)) != NULL) {
+		if (isTmp(de->d_name)) {
+			FILE* file = fopen(de->d_name);
+			compactarArchivo(file)
+			fclose(file);
+			deleteFile(de->d_name);
+		}
+	}
+}
