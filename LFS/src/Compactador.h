26e9b41c98bbddc896bc487c5398540e9cfa2793
Celeste <mederoceleste@gmail.com>
2019-06-23 23:12:47 -0300

    agrego compactarTabla

--- LFS/src/Compactador.h
+++ LFS/src/Compactador.h
@@ -1,13 +1,9 @@
-/*
- * Compactador.h
- *
- *  Created on: 16 jun. 2019
- *      Author: juanmaalt
- */
 
 #ifndef COMPACTADOR_H_
 #define COMPACTADOR_H_
 
-void compactarTabla(char* nombreTabla)
+#include "../Lissandra.h"
+
+void compactarTabla(char* nombreTabla);
 
 #endif /* COMPACTADOR_H_ */
