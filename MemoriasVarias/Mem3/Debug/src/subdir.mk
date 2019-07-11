cc348ebd3fc74b0bb04d549f1ff3dcf5dab78651
facundosalerno <facundosalerno1@gmail.com>
2019-07-10 22:13:02 -0300

    renombrada carpeta memoriasConfig

---
+++ MemoriasVarias/Mem3/Debug/src/subdir.mk
@@ -0,0 +1,39 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/ComunicacionFS.c \
+../src/Consola.c \
+../src/Gossiping.c \
+../src/ManejoDeMemoria.c \
+../src/Memoria.c \
+../src/RutinasDeLiberacion.c 
+
+OBJS += \
+./src/ComunicacionFS.o \
+./src/Consola.o \
+./src/Gossiping.o \
+./src/ManejoDeMemoria.o \
+./src/Memoria.o \
+./src/RutinasDeLiberacion.o 
+
+C_DEPS += \
+./src/ComunicacionFS.d \
+./src/Consola.d \
+./src/Gossiping.d \
+./src/ManejoDeMemoria.d \
+./src/Memoria.d \
+./src/RutinasDeLiberacion.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -I"/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
