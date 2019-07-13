a2b31199e1f14911cff3f765c63f5dae153e3c83
Celeste <mederoceleste@gmail.com>
2019-07-13 12:22:34 -0300

    Fix en funcionesAPI

--- LFS/src/adhoc/FuncionesAPI.c
+++ LFS/src/adhoc/FuncionesAPI.c
@@ -296,7 +296,7 @@ void getStringDescribe(char* path, char* pathMetadata, char* string, char* nombr
 					//printf("nombreTabla: %s\n", nombreCarpeta);
 
 					metadata = config_create(string_from_format("%s%s/Metadata", pathMetadata, nombreCarpeta));
-					char* consistencia = config_get_string_value(metadata, "CONSISTENCY");
+					string_from_format(char* consistencia = config_get_string_value(metadata, "CONSISTENCY"));
 					int compactionTime=config_get_int_value(metadata, "COMPACTION_TIME");
 					int particiones =config_get_int_value(metadata, "PARTITIONS");
 
