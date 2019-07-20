dd1e493713cefef5eaf23c8ef9152a110ec0e453
Juan M. Altamirano <juanmaalt@gmail.com>
2019-07-20 17:26:09 -0300

    Add files via upload

---
+++ Bibliotecas_compartidas/Debug/sources.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+OBJ_SRCS := 
+ASM_SRCS := 
+C_SRCS := 
+O_SRCS := 
+S_UPPER_SRCS := 
+LIBRARIES := 
+OBJS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+cliente \
+epoch \
+hash \
+parser \
+random \
+serializacion \
+server \
+server_multithread \
+
