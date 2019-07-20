701169fa6fcb439b659886f269d7805d43151b77
Juan M. Altamirano <juanmaalt@gmail.com>
2019-07-20 17:27:29 -0300

    Add files via upload

---
+++ Kernel/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lBibliotecas_compartidas -lpthread -lcommons -lreadline
+
