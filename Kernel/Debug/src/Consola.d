bd4f5e225261709617ea03eb9b0ca78144413a3b
Juan M. Altamirano <juanmaalt@gmail.com>
2019-07-20 17:41:57 -0300

    Add files via upload

---
+++ Kernel/Debug/src/Consola.d
@@ -0,0 +1,69 @@
+src/Consola.o: ../src/Consola.c ../src/Consola.h ../src/Kernel.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/cliente.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/serializacion.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../epoch/epoch.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../parser/parser_comando.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../parser/../colores/colores.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../colores/colores.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../consistencias/consistencias.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../random/random_numbers.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../colores/colores.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/parser_comando.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/colores/colores.h \
+ ../src/Planificador.h ../src/Unidad_de_ejecucion.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/random/random_numbers.h \
+ ../src/Sistema_de_criterios.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/consistencias/consistencias.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/hash/hash_sc.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/compresor_describe.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/../consistencias/consistencias.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/compresor_direccion.h \
+ ../src/Gossiping.h ../src/Metrics.h
+
+../src/Consola.h:
+
+../src/Kernel.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/cliente.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/serializacion.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../epoch/epoch.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../parser/parser_comando.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../parser/../colores/colores.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../colores/colores.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../consistencias/consistencias.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../random/random_numbers.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../colores/colores.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/parser_comando.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/colores/colores.h:
+
+../src/Planificador.h:
+
+../src/Unidad_de_ejecucion.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/random/random_numbers.h:
+
+../src/Sistema_de_criterios.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/consistencias/consistencias.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/hash/hash_sc.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/compresor_describe.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/../consistencias/consistencias.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/compresor_direccion.h:
+
+../src/Gossiping.h:
+
+../src/Metrics.h:
---
+++ Kernel/Debug/src/Planificador.d
@@ -0,0 +1,71 @@
+src/Planificador.o: ../src/Planificador.c ../src/Planificador.h \
+ ../src/Kernel.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/cliente.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/serializacion.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../epoch/epoch.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../parser/parser_comando.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../parser/../colores/colores.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../colores/colores.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../consistencias/consistencias.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../random/random_numbers.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../colores/colores.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/parser_comando.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/colores/colores.h \
+ ../src/Consola.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/random/random_numbers.h \
+ ../src/Metrics.h ../src/Unidad_de_ejecucion.h \
+ ../src/Sistema_de_criterios.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/consistencias/consistencias.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/hash/hash_sc.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/compresor_describe.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/../consistencias/consistencias.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/compresor_direccion.h \
+ ../src/Gossiping.h
+
+../src/Planificador.h:
+
+../src/Kernel.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/cliente.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/serializacion.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../epoch/epoch.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../parser/parser_comando.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../parser/../colores/colores.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../colores/colores.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../consistencias/consistencias.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../random/random_numbers.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../colores/colores.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/parser_comando.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/colores/colores.h:
+
+../src/Consola.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/random/random_numbers.h:
+
+../src/Metrics.h:
+
+../src/Unidad_de_ejecucion.h:
+
+../src/Sistema_de_criterios.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/consistencias/consistencias.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/hash/hash_sc.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/compresor_describe.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/../consistencias/consistencias.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/compresor_direccion.h:
+
+../src/Gossiping.h:
