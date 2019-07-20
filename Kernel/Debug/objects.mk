b78f331e29517338565ef118e86ceabfd2958d1e
Juan M. Altamirano <juanmaalt@gmail.com>
2019-07-20 17:26:55 -0300

    Delete objects.mk

--- Kernel/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lBibliotecas_compartidas -lpthread -lcommons -lreadline
-
