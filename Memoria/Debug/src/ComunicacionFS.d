97428b638af35ceac10f34eccb09faba191c6c99
Juan M. Altamirano <juanmaalt@gmail.com>
2019-07-20 18:03:20 -0300

    Add files via upload

---
+++ Memoria/Debug/src/ComunicacionFS.d
@@ -0,0 +1,85 @@
+src/ComunicacionFS.o: ../src/ComunicacionFS.c ../src/ComunicacionFS.h \
+ ../src/Memoria.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/colores/colores.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/cliente.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/serializacion.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../epoch/epoch.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../parser/parser_comando.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../parser/../colores/colores.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../colores/colores.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../consistencias/consistencias.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../random/random_numbers.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../colores/colores.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/parser_comando.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/server_multithread/server_multithread.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/server_multithread/../server/server.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/server_multithread/../server/../serializacion/serializacion.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/serializacion/serializacion.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/epoch/epoch.h \
+ ../src/Consola.h ../src/API/APIMemoria.h ../src/API/../Memoria.h \
+ ../src/API/UtilitariasAPI.h ../src/API/APIMemoria.h \
+ ../src/API/../ManejoDeMemoria.h ../src/API/../Memoria.h \
+ ../src/API/../ComunicacionFS.h ../src/RutinasDeLiberacion.h \
+ ../src/Gossiping.h \
+ /home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/compresor_direccion.h \
+ ../src/ManejoDeMemoria.h
+
+../src/ComunicacionFS.h:
+
+../src/Memoria.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/colores/colores.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/cliente.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/serializacion.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../epoch/epoch.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../parser/parser_comando.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../parser/../colores/colores.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../colores/colores.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../consistencias/consistencias.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../random/random_numbers.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../colores/colores.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/parser_comando.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/server_multithread/server_multithread.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/server_multithread/../server/server.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/server_multithread/../server/../serializacion/serializacion.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/serializacion/serializacion.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/epoch/epoch.h:
+
+../src/Consola.h:
+
+../src/API/APIMemoria.h:
+
+../src/API/../Memoria.h:
+
+../src/API/UtilitariasAPI.h:
+
+../src/API/APIMemoria.h:
+
+../src/API/../ManejoDeMemoria.h:
+
+../src/API/../Memoria.h:
+
+../src/API/../ComunicacionFS.h:
+
+../src/RutinasDeLiberacion.h:
+
+../src/Gossiping.h:
+
+/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/compresor_direccion.h:
+
+../src/ManejoDeMemoria.h:
