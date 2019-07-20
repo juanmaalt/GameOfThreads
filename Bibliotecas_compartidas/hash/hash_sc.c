367046509220b89ea0dc00571af2676c5c367f32
facundosalerno <facundosalerno1@gmail.com>
2019-07-19 21:52:17 -0300

    ERROR_JOURNAL agregado y fix funcion hash

--- Bibliotecas_compartidas/hash/hash_sc.c
+++ Bibliotecas_compartidas/hash/hash_sc.c
@@ -8,7 +8,7 @@
 #include "hash_sc.h"
 
 hash getHash(char *input, unsigned int limite){
-	if(limite < 1)
+	if(limite <= 1) //Si es 0, chau. Si es 1 significan dos cosas: Que hay una sola memoria por ende respondo con 0 que seria el primer elemento de la lista. Y ademas que cuando tenga que hacer el resto, me daria division por 0 por que hace limite-1
 		return 0;
 	int acumulador = 1;
 	for(int i=0; i<strlen(input); ++i){
