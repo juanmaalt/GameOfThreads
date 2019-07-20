d65a910e27c81387dd1be7cc50590c5fbe2d4099
Juan M. Altamirano <juanmaalt@gmail.com>
2019-07-20 17:40:41 -0300

    Add files via upload

--- Memoria/Debug/src/subdir.mk
+++ Memoria/Debug/src/subdir.mk
@@ -4,32 +4,20 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../src/ComunicacionFS.c \
-../src/Consola.c \
-../src/Gossiping.c \
-../src/ManejoDeMemoria.c \
-../src/Memoria.c \
-../src/RutinasDeLiberacion.c 
+../src/API/APIMemoria.c \
+../src/API/UtilitariasAPI.c 
 
 OBJS += \
-./src/ComunicacionFS.o \
-./src/Consola.o \
-./src/Gossiping.o \
-./src/ManejoDeMemoria.o \
-./src/Memoria.o \
-./src/RutinasDeLiberacion.o 
+./src/API/APIMemoria.o \
+./src/API/UtilitariasAPI.o 
 
 C_DEPS += \
-./src/ComunicacionFS.d \
-./src/Consola.d \
-./src/Gossiping.d \
-./src/ManejoDeMemoria.d \
-./src/Memoria.d \
-./src/RutinasDeLiberacion.d 
+./src/API/APIMemoria.d \
+./src/API/UtilitariasAPI.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-src/%.o: ../src/%.c
+src/API/%.o: ../src/API/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
 	gcc -I"/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
