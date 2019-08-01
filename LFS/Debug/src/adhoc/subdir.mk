3d22291c10ec8c05a97578200d76566df12207b5
facundosalerno <facundosalerno1@gmail.com>
2019-08-01 00:04:33 -0300

    mk

--- LFS/Debug/src/adhoc/subdir.mk
+++ LFS/Debug/src/adhoc/subdir.mk
@@ -8,28 +8,31 @@ C_SRCS += \
 ../src/adhoc/Bitmap.c \
 ../src/adhoc/Consola.c \
 ../src/adhoc/FuncionesAPI.c \
-../src/adhoc/FuncionesComp.c 
+../src/adhoc/FuncionesComp.c \
+../src/adhoc/Semaforos.c 
 
 OBJS += \
 ./src/adhoc/APILissandra.o \
 ./src/adhoc/Bitmap.o \
 ./src/adhoc/Consola.o \
 ./src/adhoc/FuncionesAPI.o \
-./src/adhoc/FuncionesComp.o 
+./src/adhoc/FuncionesComp.o \
+./src/adhoc/Semaforos.o 
 
 C_DEPS += \
 ./src/adhoc/APILissandra.d \
 ./src/adhoc/Bitmap.d \
 ./src/adhoc/Consola.d \
 ./src/adhoc/FuncionesAPI.d \
-./src/adhoc/FuncionesComp.d 
+./src/adhoc/FuncionesComp.d \
+./src/adhoc/Semaforos.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/adhoc/%.o: ../src/adhoc/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I"/home/utnso/workspace/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	gcc -I"/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
