c0ae762916a1a66fec146ac916014f83b7b4f313
facundosalerno <facundosalerno1@gmail.com>
2019-08-01 00:38:42 -0300

    Add files via upload

---
+++ LFS/Debug/src/FileSystem.d
@@ -0,0 +1,85 @@
+src/FileSystem.o: ../src/FileSystem.c ../src/FileSystem.h \
+ ../src/Lissandra.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/cliente.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/serializacion.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../epoch/epoch.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../parser/parser_comando.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../parser/../colores/colores.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../colores/colores.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../consistencias/consistencias.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../random/random_numbers.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../colores/colores.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/parser_comando.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/server_multithread/server_multithread.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/server_multithread/../server/server.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/server_multithread/../server/../serializacion/serializacion.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/epoch/epoch.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/colores/colores.h \
+ ../src/adhoc/Consola.h ../src/adhoc/../Lissandra.h \
+ ../src/adhoc/APILissandra.h ../src/adhoc/FuncionesAPI.h \
+ ../src/adhoc/Metadata.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/compresor_describe.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/../consistencias/consistencias.h \
+ ../src/adhoc/Bitmap.h ../src/adhoc/Semaforos.h ../src/Compactador.h \
+ ../src/adhoc/FuncionesComp.h ../src/adhoc/APILissandra.h \
+ ../src/adhoc/Consola.h
+
+../src/FileSystem.h:
+
+../src/Lissandra.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/cliente.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/serializacion.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../epoch/epoch.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../parser/parser_comando.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../parser/../colores/colores.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../colores/colores.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../consistencias/consistencias.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../random/random_numbers.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../colores/colores.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/parser_comando.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/server_multithread/server_multithread.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/server_multithread/../server/server.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/server_multithread/../server/../serializacion/serializacion.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/epoch/epoch.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/colores/colores.h:
+
+../src/adhoc/Consola.h:
+
+../src/adhoc/../Lissandra.h:
+
+../src/adhoc/APILissandra.h:
+
+../src/adhoc/FuncionesAPI.h:
+
+../src/adhoc/Metadata.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/compresor_describe.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/../consistencias/consistencias.h:
+
+../src/adhoc/Bitmap.h:
+
+../src/adhoc/Semaforos.h:
+
+../src/Compactador.h:
+
+../src/adhoc/FuncionesComp.h:
+
+../src/adhoc/APILissandra.h:
+
+../src/adhoc/Consola.h:
---
+++ LFS/Debug/src/adhoc/Consola.d
@@ -0,0 +1,90 @@
+src/adhoc/Consola.o: ../src/adhoc/Consola.c ../src/adhoc/Consola.h \
+ ../src/adhoc/../Lissandra.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/cliente.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/serializacion.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../epoch/epoch.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../parser/parser_comando.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../parser/../colores/colores.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../colores/colores.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../consistencias/consistencias.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../random/random_numbers.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../colores/colores.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/parser_comando.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/server_multithread/server_multithread.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/server_multithread/../server/server.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/server_multithread/../server/../serializacion/serializacion.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/epoch/epoch.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/colores/colores.h \
+ ../src/adhoc/../adhoc/Consola.h ../src/adhoc/../adhoc/APILissandra.h \
+ ../src/adhoc/../adhoc/FuncionesAPI.h \
+ ../src/adhoc/../adhoc/../Lissandra.h ../src/adhoc/../adhoc/Metadata.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/compresor_describe.h \
+ /home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/../consistencias/consistencias.h \
+ ../src/adhoc/../adhoc/Bitmap.h ../src/adhoc/../FileSystem.h \
+ ../src/adhoc/../Lissandra.h ../src/adhoc/../adhoc/Semaforos.h \
+ ../src/adhoc/../Compactador.h ../src/adhoc/../adhoc/FuncionesComp.h \
+ ../src/adhoc/../adhoc/APILissandra.h ../src/adhoc/../adhoc/Consola.h
+
+../src/adhoc/Consola.h:
+
+../src/adhoc/../Lissandra.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/cliente.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/serializacion.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../epoch/epoch.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../parser/parser_comando.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../parser/../colores/colores.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../colores/colores.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../consistencias/consistencias.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../serializacion/../random/random_numbers.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/cliente/../colores/colores.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/parser_comando.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/server_multithread/server_multithread.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/server_multithread/../server/server.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/server_multithread/../server/../serializacion/serializacion.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/epoch/epoch.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/colores/colores.h:
+
+../src/adhoc/../adhoc/Consola.h:
+
+../src/adhoc/../adhoc/APILissandra.h:
+
+../src/adhoc/../adhoc/FuncionesAPI.h:
+
+../src/adhoc/../adhoc/../Lissandra.h:
+
+../src/adhoc/../adhoc/Metadata.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/compresor_describe.h:
+
+/home/facusalerno/workspace-tp-2019/tp-2019-1c-GameOfThreads/Bibliotecas_compartidas/parser/../consistencias/consistencias.h:
+
+../src/adhoc/../adhoc/Bitmap.h:
+
+../src/adhoc/../FileSystem.h:
+
+../src/adhoc/../Lissandra.h:
+
+../src/adhoc/../adhoc/Semaforos.h:
+
+../src/adhoc/../Compactador.h:
+
+../src/adhoc/../adhoc/FuncionesComp.h:
+
+../src/adhoc/../adhoc/APILissandra.h:
+
+../src/adhoc/../adhoc/Consola.h:
